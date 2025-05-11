bda52313b3f54bb74c1d141ebc1f8a705aa7505c
Gabriela Yasmin <gabitagua36@gmail.com>
2025-05-10 23:40:41 -0300

    Update desafio1.py

---
+++ Curso-em-video/Mundo1/desafios/desafio1.py
@@ -0,0 +1,5 @@
+#Crie um programa que leia dois numeros e mostre a soma entre eles:
+x = int (input('primeiro numero:'))
+y = int (input('segundo numero:'))
+soma = x + y
+print('a soma entre {} e {} vale {}'.format(x,y, soma))
